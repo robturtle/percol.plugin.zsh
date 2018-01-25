2fc4a53d3112f37dc395b0c82ba34ec8ba3aa3c9
Yang Liu <jeremyrobturtle@gmail.com>
2018-01-25 10:46:35 -0800

    rake migrate/down functions

--- percol.plugin.zsh
+++ percol.plugin.zsh
@@ -18,13 +18,29 @@ if exists percol; then
     bindkey '^R' percol_select_history
     
     function percol_git_checkout() {
-        local tac
         br=`git branch | percol | sed 's/*//' | sed 's/ //g'`
+        test -z $br && return 1
         git checkout $br >/dev/null
         zle -R -c
     }
-    zle -N percol_git_checkout
-    bindkey '^T' percol_git_checkout
+    bindkey -s '^T' 'percol_git_checkout\n'
+
+    function percol_rake_down() {
+        version=`ls db/migrate | tail -r | percol | sed 's/\.rb$//'`
+        test -z $version && return 1
+        echo "reverting test db..."
+        rake db:migrate:down VERSION=$version RAILS_ENV=test
+        echo "reverting development db..."
+        rake db:migrate:down VERSION=$version
+    }
+    bindkey -s '^U' 'percol_rake_down\n'
+
+    function rake_migrate() {
+        echo "migrating test db..."
+        rake db:migrate:with_data RAILS_ENV=test
+        echo "migrating development db..."
+        rake db:migrate:with_data
+    }
 
     ## use `fg 1` just like under BASH, no need to type `fg %1`
     fg() {
